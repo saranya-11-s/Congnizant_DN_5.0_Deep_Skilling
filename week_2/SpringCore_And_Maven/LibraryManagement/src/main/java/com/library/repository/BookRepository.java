96927b13dd9d0a54ce09a74bb4e1e2175636bf32
Saranya11 <saranya11edu@gmail.com>
2026-06-28 10:09:25 +0530

    Add files via upload

---
+++ week_2/SpringCore_And_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public String getBookDetails() {
+        return "Book information retrieved successfully from the repository.";
+    }
+}
