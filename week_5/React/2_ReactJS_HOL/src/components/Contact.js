3bd4dcf8c64519287227f944619b33d90b07c942
Saranya11 <saranya11edu@gmail.com>
2026-07-12 23:17:45 +0530

    Add files via upload

---
+++ week_5/React/2_ReactJS_HOL/src/components/Contact.js
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>Welcome to the Contact page of the Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ week_5/React/2_ReactJS_HOL/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>Welcome to the Home page of Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default Home;
