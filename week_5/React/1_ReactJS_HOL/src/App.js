9f3cba3f9ab41c59aad344e1a57e13cb5d085947
Saranya11 <saranya11edu@gmail.com>
2026-07-12 22:38:32 +0530

    Add files via upload

---
+++ week_5/React/1_ReactJS_HOL/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "40px"
+      }}
+    >
+      <h1>Welcome to the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
