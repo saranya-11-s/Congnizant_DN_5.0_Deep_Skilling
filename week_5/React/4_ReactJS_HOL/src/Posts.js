7caa6f7d66f0389ef670c9272d9bf16bb6eefd41
Saranya11 <saranya11edu@gmail.com>
2026-07-13 00:10:07 +0530

    Add files via upload

---
+++ week_5/React/4_ReactJS_HOL/src/Posts.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  loadPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ posts: data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error) {
+    alert(error);
+  }
+
+  render() {
+    return (
+      <div style={{ margin: "20px" }}>
+        <h1>Blog Posts</h1>
+
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
